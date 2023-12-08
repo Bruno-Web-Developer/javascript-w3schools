8ac461821427376b7d66a39af69de2b06f6f8df3
BrunoMC <correabrunomiguel@gmail.com>
2023-12-08 09:12:22 -0300

    Iniciado

---
+++ 01-basico/js/09.js
@@ -0,0 +1,10 @@
+/* Arrumar função */
+
+function myExample1() {
+    let p1 = document.getElementById('var1').value
+    let p2 = document.getElementById('var2').value
+
+    let res = p1 * p2
+    
+    document.getElementById('res1').value = res 
+}
